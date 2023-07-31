35b0fa8863af52dc37800223eff888d41dd14ed2
minseokKim0501 <klm7637@naver.com>
2023-07-31 23:28:12 +0900

    날짜 : 2023-07-31 이름 : 김민석 내용 : 스프링 빈 검증 프로젝트 준비

--- src/test/java/hello/itemservice/validation/BeanValidationTest.java
+++ src/test/java/hello/itemservice/validation/BeanValidationTest.java
@@ -1,6 +1,33 @@
 package hello.itemservice.validation;
 
+import hello.itemservice.domain.item.Item;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
 public class BeanValidationTest {
 
+    @Test
+    void beanValidation() {
+        // 스프링과 통합하면 직접 작성할 일 없음
+        // 빈 검증기 직접 활용 방법
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        Item item = new Item();
+        item.setItemName(" ");
+        item.setPrice(0);
+        item.setQuantity(10000);
+
+        Set<ConstraintViolation<Item>> violations =  validator.validate(item);
+        for (ConstraintViolation<Item> violation : violations) {
+            System.out.println("violation = " + violation);
+            System.out.println("violation = " + violation.getMessage());
+        }
+    }
 
 }
